9d70d1762d8a24441cc6ef47d3add72bff493634
FXTD-Odyssey <820472580@qq.com>
2019-11-06 13:56:17 +0800

    update

--- PyMel/extractPymelHtml.py
+++ PyMel/extractPymelHtml.py
@@ -22,7 +22,7 @@ pymel_module = ["pymel.core.datatypes",
 # for module in pymel_module:
 #     html_dict[module] = []
 
-html_dict = {}
+html_dict = {"": [], "functions": {}, "classes": {}}
 module_list = []
 for root, dirs, files in os.walk(HTML):
     for html in files:
@@ -32,28 +32,28 @@ for root, dirs, files in os.walk(HTML):
             for module in pymel_module:
                 if module in html and module != html:
                     module,_,func = html.rpartition(".")
-                    if not html_dict.has_key(module):
-                        html_dict[module] = {"functions": [], "classes":[]}
+                    # if not html_dict.has_key(module):
+                    #     html_dict[module] = {"functions": [], "classes":[]}
                     TYPE = root.split(HTML)[-1][1:].replace("\\","/").split("/")[0]
-                    # print TYPE, root.split(HTML)[-1][1:]
                     # NOTE  如果 TYPE 为空，说明属于模块
                     if TYPE == '':
                         module_list.append(html)
                         break
 
-                    html_dict[module][TYPE].append(html)
+                    if not html_dict[TYPE].has_key(module):
+                        html_dict[TYPE][module] = []
+                    
+
+                    html_dict[TYPE][module].append(html)
 
                     break
 
-print module_list
+
 for module in module_list:
-    if module not in html_dict:
-        print(module)
-        # html_dict[module] = 
+    if module not in html_dict["functions"] and module not in html_dict["classes"]:
+        html_dict[""].append(module)
 
-# print (html_dict)
-
-# path = os.path.join(DIR,"html2.json")
-# with open(path,'w') as f:
-#     json.dump(html_dict,f,indent=4)
+path = os.path.join(DIR,"html3.json")
+with open(path,'w') as f:
+    json.dump(html_dict,f,indent=4)
 
